6795b96ef1e19c82b05d9e394c4d4082e70ed300
Jackson93150 <jackjack93150@gmail.com>
2021-11-09 16:02:34 +0100

    premier ennemie

--- js/prg.js
+++ js/prg.js
@@ -12,82 +12,124 @@ feu.img.onload = function () {
 };
 let keysPressed = {};
 let background = new Image();
-background.src = "./assets/background.png"
+background.src = "./assets/background.png";
 let background2 = new Image();
-background2.src = "./assets/background.png"
+background2.src = "./assets/background.png";
 let bgx = 0;
-let bgx2 = background.width*1.4;
+let bgx2 = background.width * 1.4;
 let bgs = 2;
 let tirvec = [];
+let alienvec = [];
 
-function backgloop(){
-  if(bgx2 == 0){
-    bgx = background.width*1.4;
+function getRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function spawnAlien(){
+  let y = getRandom(0,cnv.height-82);
+  let ali = new Sprite(82, 62, cnv.width, y, 20, 1);
+  ali.img.src = "./assets/alien.png";
+  ali.img.onload = function () {
+    ali.load();
+  };
+  alienvec.push(ali);
+}
+
+function moveAlien(i){
+  alienvec[i].posX -= 5;
+}
+
+function backgloop() {
+  if (bgx2 == 0) {
+    bgx = background.width * 1.4;
   }
-  if(bgx == 0){
-    bgx2 = background.width*1.4;
+  if (bgx == 0) {
+    bgx2 = background.width * 1.4;
   }
 }
 
-function tirstart(){
-  let tir = new Tir(perso.posx+30,perso.posy+28);
+function tirstart() {
+  let tir = new Tir(perso.posx + 30, perso.posy + 28);
   tirvec.push(tir);
 }
-
 
 document.addEventListener("keydown", (event) => {
   keysPressed[event.key] = true;
 
-  if(keysPressed["z"] && event.key == "d" && event.key == "j" || keysPressed["d"] && event.key == "z" && event.key == "j"){
+  if (
+    (keysPressed["z"] && event.key == "d" && event.key == "j") ||
+    (keysPressed["d"] && event.key == "z" && event.key == "j")
+  ) {
     perso.fly();
     perso.fly_right();
     tirstart();
   }
-  if(keysPressed["z"] && event.key == "q" && event.key == "j" || keysPressed["q"] && event.key == "z" && event.key == "j"){
+  if (
+    (keysPressed["z"] && event.key == "q" && event.key == "j") ||
+    (keysPressed["q"] && event.key == "z" && event.key == "j")
+  ) {
     perso.fly();
     perso.fly_left();
     tirstart();
   }
-  if(keysPressed["s"] && event.key == "d" && event.key == "j" || keysPressed["d"] && event.key == "s" && event.key == "j"){
+  if (
+    (keysPressed["s"] && event.key == "d" && event.key == "j") ||
+    (keysPressed["d"] && event.key == "s" && event.key == "j")
+  ) {
     perso.stop_fly();
     perso.fly_right();
     tirstart();
   }
-  if(keysPressed["s"] && event.key == "q" && event.key == "j" || keysPressed["q"] && event.key == "s" && event.key == "j"){
+  if (
+    (keysPressed["s"] && event.key == "q" && event.key == "j") ||
+    (keysPressed["q"] && event.key == "s" && event.key == "j")
+  ) {
     perso.stop_fly();
     perso.fly_left();
     tirstart();
   }
 
-  if(keysPressed["z"] && event.key == "d"  || keysPressed["d"] && event.key == "z" ){
+  if (
+    (keysPressed["z"] && event.key == "d") ||
+    (keysPressed["d"] && event.key == "z")
+  ) {
     perso.fly();
     perso.fly_right();
   }
-  if(keysPressed["z"] && event.key == "q" || keysPressed["q"] && event.key == "z" ){
+  if (
+    (keysPressed["z"] && event.key == "q") ||
+    (keysPressed["q"] && event.key == "z")
+  ) {
     perso.fly();
     perso.fly_left();
   }
-  if(keysPressed["s"] && event.key == "d" || keysPressed["d"] && event.key == "s" ){
+  if (
+    (keysPressed["s"] && event.key == "d") ||
+    (keysPressed["d"] && event.key == "s")
+  ) {
     perso.stop_fly();
     perso.fly_right();
   }
-  if(keysPressed["s"] && event.key == "q" || keysPressed["q"] && event.key == "s" ){
+  if (
+    (keysPressed["s"] && event.key == "q") ||
+    (keysPressed["q"] && event.key == "s")
+  ) {
     perso.stop_fly();
     perso.fly_left();
   }
-  if(keysPressed["j"]){
+  if (keysPressed["j"]) {
     tirstart();
   }
-  if(keysPressed["z"]){
+  if (keysPressed["z"]) {
     perso.fly();
   }
-  if(keysPressed["s"]){
+  if (keysPressed["s"]) {
     perso.stop_fly();
   }
-  if(keysPressed["d"]){
+  if (keysPressed["d"]) {
     perso.fly_right();
   }
-  if(keysPressed["q"]){
+  if (keysPressed["q"]) {
     perso.fly_left();
   }
 });
@@ -101,10 +143,22 @@ function update() {
   cnv.height = window.innerHeight;
   ctx.clearRect(0, 0, cnv.width, cnv.height);
   backgloop();
-  ctx.drawImage(background,bgx,0,background.width*1.4,background.height*1.4);
-  ctx.drawImage(background2,bgx2,0,background.width*1.4,background.height*1.4);
-  bgx-= bgs;
-  bgx2-= bgs;
+  ctx.drawImage(
+    background,
+    bgx,
+    0,
+    background.width * 1.4,
+    background.height * 1.4
+  );
+  ctx.drawImage(
+    background2,
+    bgx2,
+    0,
+    background.width * 1.4,
+    background.height * 1.4
+  );
+  bgx -= bgs;
+  bgx2 -= bgs;
   perso.limite(cnv.height);
   perso.limite2(0);
   perso.limite3(0);
@@ -114,17 +168,23 @@ function update() {
   feu.posY = perso.posy + 40;
   feu.draw();
   perso.draw();
-  for(let i = 0 ; i < tirvec.length;i++){
+  for (let i = 0; i < tirvec.length; i++) {
     tirvec[i].draw(cnv.width);
     tirvec[i].move();
-    if(tirvec[i].state == false){
+    if (tirvec[i].state == false) {
       tirvec.splice(i, 1);
     }
-  }
+  };
+  for(let i = 0; i < alienvec.length; i++) {
+    alienvec[i].draw();
+    moveAlien(i);
+  };
 
   setTimeout(() => {
     requestAnimationFrame(update);
   }, 1000 / fps);
 }
+
+setInterval(spawnAlien,1000);
 
 update();
